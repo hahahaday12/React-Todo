fef538ae081ec92e836d11426c0ebe6f04d4e32f
hahahaday12 <hahahaday12@naver.com>
2023-05-24 19:56:50 +0900

    feat : Modal componts
    - 음악 앨범이 있는 모달 컴포넌트

---
+++ src/components/views/components/Modal/modal.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { useEffect } from 'react'
+import './modal.scss'
+import { musicState  } from '../../../../store/store';
+import { useRecoilState } from 'recoil';
+import { getYoutube } from '../../../../utils/apis/apis';
+
+function ModalOpen () {
+
+  const [musicList, setMusicList] = useRecoilState(musicState);
+
+  const [music, setMusic] = useState([]);
+  const [link, setLink] = useState("");
+  const [play, setPlay] = useState(false);
+
+  async function createList() {
+    try {
+      const param = await getYoutube();
+      console.log("end");
+      setMusic(param.items);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    createList();
+  },[]);
+
+  const musicEvent = (key, img) => {
+    let play = false;
+    let paths = "";
+
+    const path = "https://www.youtu.be/"+key;
+    if(path === link) {
+      play = false;
+        setPlay(false);
+        setLink("");
+    } else{
+      play = true;
+        setLink(path);
+        setPlay(true);
+        paths = path;``
+    }
+
+    const musicInfo = {
+      playStatus : play,
+      thumNailImag : img,
+      youtubeLink: paths,
+    };
+
+    setMusicList(musicInfo);
+  }
+
+  return(
+    <div className="ModalContainer">
+      {music && music.map((obj) => (
+        <div className='music_box'>
+          <img src={obj.snippet.thumbnails.maxres.url} 
+          onClick={() => musicEvent(obj.snippet.resourceId.videoId, obj.snippet.thumbnails.maxres.url)}/>        
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ModalOpen
